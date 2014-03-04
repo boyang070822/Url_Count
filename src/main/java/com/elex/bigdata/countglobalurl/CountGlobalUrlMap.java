528a784363416a3cc07d793b107da45ffac62434
boyang070822 <boyang070822@gmail.com>
2014-03-04 13:59:33 +0800

    change Mapper

--- src/main/java/com/elex/bigdata/countglobalurl/CountGlobalUrlMap.java
+++ src/main/java/com/elex/bigdata/countglobalurl/CountGlobalUrlMap.java
@@ -14,9 +14,9 @@ import java.io.IOException;
  * Time: 3:16 PM
  * To change this template use File | Settings | File Templates.
  */
-public class CountGlobalUrlMap extends Mapper<Text,Text,Text,Text> {
+public class CountGlobalUrlMap extends Mapper<Object,Text,Text,Text> {
      private static Logger logger=Logger.getLogger(CountGlobalUrlMap.class);
-  public void map(Text key,Text value,Context context) throws IOException, InterruptedException {
+  public void map(Object key,Text value,Context context) throws IOException, InterruptedException {
      //get the url and count
      String[] fields=value.toString().split("\t");
      for(int i=0;i<fields.length;i++)
