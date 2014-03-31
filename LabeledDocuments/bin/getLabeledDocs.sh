16008c85ce5a5309af6b75c37f9b5d524e3db89e
boyang070822 <boyang070822@gmail.com>
2014-03-31 11:28:59 +0800

    get labeledDocs use day as time unit

--- LabeledDocuments/bin/getLabeledDocs.sh
+++ LabeledDocuments/bin/getLabeledDocs.sh
@@ -2,7 +2,7 @@ baseDir=`dirname $0`/..
 echo $0,$baseDir
 inputDir=/user/hadoop/url_count/$1
 outputDir=/user/hadoop/url_count/labeledDocs/$2
-localOutputFile=/data/log/user_category/llda/labeledDocs.$2
+localOutputFile=/data/log/user_category/llda/labeledDocs/$2
 Jar=$baseDir/target/LabeledDocuments-jar-with-dependencies.jar
 Main=com.elex.bigdata.labeledDocuments.LabeledDocument
 echo "hadoop fs -rm -r $outputDir"
--- bin/getDocs.sh
+++ bin/getDocs.sh
@@ -2,4 +2,4 @@ startTime=$1
 endTime=$2
 baseDir=`dirname $0`/..
 sh $baseDir/bin/CountUidUrl.sh $startTime $endTime
-sh $baseDir/LabeledDocuments/bin/getLabeledDocs.sh ${startTime}_${endTime} ${startTime}_${endTime}
+sh $baseDir/LabeledDocuments/bin/getLabeledDocs.sh ${startTime:0:8}* ${startTime:0:8}
