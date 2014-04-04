39339a018743dfb25952e6f804b84b74a1585c1a
boyang070822 <boyang070822@gmail.com>
2014-04-04 18:08:49 +0800

    getLabeledDocs.sh

--- LabeledDocuments/bin/getLabeledDocs.sh
+++ LabeledDocuments/bin/getLabeledDocs.sh
@@ -1,14 +1,12 @@
 baseDir=`dirname $0`/..
 echo $0,$baseDir
-inputDir=/user/hadoop/url_count/$1
-outputDir=/user/hadoop/url_count/labeledDocs/$2
-localOutputFile=/data/log/user_category/llda/labeledDocs/$2
+inputTime=$1
+outputTime=$2
+inputDir=/user/hadoop/url_count
+outputDir=/user/hadoop/user_category/llda/docs
+localOutputFile=/data/user_category/llda/docs
 Jar=$baseDir/target/LabeledDocuments-jar-with-dependencies.jar
 Main=com.elex.bigdata.labeledDocuments.LabeledDocument
-echo "hadoop fs -rm -r $outputDir"
-hadoop fs -rm -r $outputDir
-logFile=/data/log/user_category/processLog/getLabeledDocs.log
-echo "hadoop jar $Jar $Main $inputDir $outputDir >> $logFile 2>&1"
-hadoop jar $Jar $Main $inputDir $outputDir >> $logFile 2>&1
-echo "hadoop fs -getmerge $outputDir $localOutputFile"
-hadoop fs -getmerge $outputDir $localOutputFile
+logFile=/data/user_category/llda/logs/getLabeledDocs.log
+echo "hadoop jar $Jar $Main -inputBase $inputDir -outputBase $outputDir -inputTime $inputTime -outputTime $outputTime  >> $logFile 2>&1"
+hadoop jar $Jar $Main -inputBase $inputDir -outputBase $outputDir -inputTime $inputTime -outputTime $outputTime  >> $logFile 2>&1
