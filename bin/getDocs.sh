e2ac45b3d9fb7e9de9cf43035b28034927151cdc
boyang070822 <boyang070822@gmail.com>
2014-04-14 14:18:47 +0800

    use Accumulate to get url count; scan only once per five minutes

--- bin/getDocs.sh
+++ bin/getDocs.sh
@@ -1,5 +1,5 @@
 startTime=$1
 endTime=$2
 baseDir=`dirname $0`/..
-sh $baseDir/bin/CountUidUrl.sh $startTime $endTime
+sh $baseDir/bin/AccumulateUrlCount.sh $startTime $endTime
 sh $baseDir/LabeledDocuments/bin/getLabeledDocs.sh ${startTime:0:8}* ${startTime:0:8}
