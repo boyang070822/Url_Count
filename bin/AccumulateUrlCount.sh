f2351d8f144b18890052de6a5583cae207bfa8d8
boyang070822 <boyang070822@gmail.com>
2014-04-14 10:16:48 +0800

    Accumulate.sh

--- bin/AccumulateUrlCount.sh
+++ bin/AccumulateUrlCount.sh
@@ -5,6 +5,6 @@ endTime=$2
 CountUidUrlJar=$baseDir/CountUidUrl/target/Count_UidUrl-jar-with-dependencies.jar
 CountUidUrlMain=com.elex.bigdata.urlcount.Accumulate
 outputBase=/user/hadoop/url_count/
-echo "hadoop jar $CountUidUrlJar $CountUidUrlMain -outputBase $outputBase -startTime $startTime -endTime $endTime >> $logFile 2>&1"
 logFile=/data/user_category/llda/logs/AccumulateUidUrl.log
+echo "hadoop jar $CountUidUrlJar $CountUidUrlMain -outputBase $outputBase -startTime $startTime -endTime $endTime >> $logFile 2>&1"
 hadoop jar $CountUidUrlJar $CountUidUrlMain -outputBase $outputBase -startTime $startTime -endTime $endTime >> $logFile 2>&1
